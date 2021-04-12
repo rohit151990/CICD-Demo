a1c87be15d5f62674d29730930ad33d86508f768
unknown <rohit151990>
2021-04-12 13:58:38 +0530

    a

--- scripts/fetch_gae_sdk.py
+++ scripts/fetch_gae_sdk.py
@@ -23,9 +23,9 @@ Current releases are listed here:
 
 import json
 import os
-import StringIO
+from io import StringIO
 import sys
-import urllib2
+from urllib.request import urlopen
 import zipfile
 
 _SDK_URL = (
@@ -34,7 +34,7 @@ _SDK_URL = (
 
 def get_gae_versions():
     try:
-        version_info_json = urllib2.urlopen(_SDK_URL).read()
+        version_info_json = urlopen(_SDK_URL).read()
     except:
         return {}
     try:
@@ -77,7 +77,7 @@ def main(argv):
     sdk_urls = get_sdk_urls(sdk_versions)
     for sdk_url in sdk_urls:
         try:
-            sdk_contents = StringIO.StringIO(urllib2.urlopen(sdk_url).read())
+            sdk_contents = StringIO(urlopen(sdk_url).read())
             break
         except:
             pass
